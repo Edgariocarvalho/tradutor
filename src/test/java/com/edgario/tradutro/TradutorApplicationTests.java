f47893c3427b0f2d585aeada28af2571c88216e6
Edgariocarvalho <edgariocarvalhoow@gmail.com>
2021-08-16 11:28:32 +0100

    commite inicial

---
+++ src/test/java/com/edgario/tradutro/TradutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgario.tradutro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
